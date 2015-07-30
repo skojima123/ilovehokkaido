eb0eea08da2a21259142c95b59239d9dc34dbccf
skj37 <kojima37@gmail.com>
2015-07-30 22:23:11 +0900

    latest update

--- map-angular.js
+++ map-angular.js
@@ -1,25 +1,13 @@
 var module = angular.module('hokkaido_map', []);
 
-module.controller('MapCtrl', function ($scope, $http) {
 
-    $scope.selectedCity = "***";
-    $scope.data = ["旭川市", "根室市", "稚内市"];
-
-
-    var loadData = function () {
-        var uri = "./Hokkaido/hokkaido_population_delta.json";
-        $http.get(uri)
-            .success(function (data, status) {
-                //console.log(data);
-                $scope.dataSet = data;
-
-            })
-            .error(function (data, status, error) {
-
-            });
-
+module.directive('viewDirective', function(){
+    return {
+        template: '<h1>HokkaidoMapView</h1>'
     };
+});
 
+module.controller('MapCtrl', function ($scope, $http) {
 
     var m_width = $('#map').width(),
         width = 1000,
@@ -32,19 +20,14 @@ module.controller('MapCtrl', function ($scope, $http) {
         .attr('viewBox', '0 0 ' + width + ' ' + height)
         .attr('width', m_width)
         .attr('height', m_width * height / width);
-//.append('g');
-
-//map.append('rect')
-//    .attr('class','background')
-//    .attr('width',width)
-//    .attr('height',height)
-
-//map.append('g')
 
 
-    $scope.populationById = d3.map()
+
 
     $scope.populationDeltaChange = d3.map();
+
+    $scope.mapper = {};
+    $scope.data = [];
 
     var onMouseOver = function (d) {
         var xy = d3.mouse(document.getElementById('map'));
@@ -63,6 +46,7 @@ module.controller('MapCtrl', function ($scope, $http) {
                 .html("<li>" + d.id + "</li>" +
                 "<li>人口推移:" + sign + num.toString() + "%</li>");
 
+            console.log(this);
             d3.select(this)
                 .attr('fill-opacity', 0.4)
                 .style('stroke-width', 1)
@@ -75,8 +59,6 @@ module.controller('MapCtrl', function ($scope, $http) {
         d3.select('#tooltip')
             .classed('hidden', true)
             .text('');
-        //$(this).attr('border', '1px solid black');
-        //$(this).attr("fill-opacity", 1.0);
         d3.select(this)
             .attr('fill-opacity', 1.0)
             .style('stroke', 'black')
@@ -84,26 +66,48 @@ module.controller('MapCtrl', function ($scope, $http) {
     }
 
 
-// order light -> dark
     var colorPaletteNegative = ["#E8EAF6", "#C5CAE9", "#9FA8DA", "#7986CB", "#5C6BC0", "#3F51B5", "#3949AB", "#303F9F", "#283593", "#1A237E"];
     var colorPalettePositive = ["#FFEBEE", "#FFCDD2", "#EF9A9A", "#E57373", "#EF5350", "#F44336", "#E53935", "#D32F2F", "#C62828", "#B71C1C"];
 
     queue(1)
         .defer(d3.json, "./Hokkaido/hokkaido_v4.topojson")
-        .defer(d3.tsv, "./Hokkaido/population.tsv", function (d) {
-            $scope.populationById.set(d.city_name, d.population)
-
-        })
         .defer(d3.csv, "./Hokkaido/hokkaido_population_delta.csv", function (d) {
             //console.log(d.delta);
-            $scope.populationDeltaChange.set(d.city, Number(d.delta).toFixed(1));
+            $scope.populationDeltaChange.set(d.city, [d.population, Number(d.delta).toFixed(1)]);
+            $scope.mapper[d.city]=[d.population, Number(d.delta).toFixed(1)];
         })
         .await(ready);
 
+    $scope.rowClicked = function(k){
+        console.log("clicked: "+k);
+        if (k!==undefined){
+            var pathId = "#"+k;
+            d3.selectAll(pathId)
+                .attr('fill-opacity', 0.4)
+                .style('stroke-width', 1)
+                .style('stroke', '#827717');
+        }
+    }
 
-    function ready(error, o) {
-        //console.log(o);
-        var center = d3.geo.centroid(topojson.feature(o, o.objects.hokkaido))
+    $scope.rowLeft = function(k){
+        if (k!==undefined){
+            var pathId = "#"+k;
+            d3.selectAll(pathId)
+                .attr('fill-opacity', 1.0)
+                .style('stroke', 'black')
+                .style('stroke-width', 0.1);
+        }
+    }
+
+    function ready(error, topo) {
+
+        console.log('rendering map');
+        //$scope.mapper = $scope.populationDeltaChange.entries();
+        //console.log($scope.mapper);
+
+        $scope.$apply();
+
+        var center = d3.geo.centroid(topojson.feature(topo, topo.objects.hokkaido))
         //console.log(center);
         var projection = d3.geo.mercator()
             .center(center)
@@ -114,14 +118,11 @@ module.controller('MapCtrl', function ($scope, $http) {
             .projection(projection);
 
         var max = d3.max($scope.populationDeltaChange.values(), function (d) {
-            return Number(d);
+            return Number(d[1]);
         });
         var min = d3.min($scope.populationDeltaChange.values(), function (d) {
-            return Number(d);
+            return Number(d[1]);
         });
-        //console.log(maxPopulation);
-        //console.log(max);
-        //console.log(min);
 
         var quantize = d3.scale.quantize()
             .domain([min, max])
@@ -138,23 +139,29 @@ module.controller('MapCtrl', function ($scope, $http) {
             .range(colorPaletteNegative.reverse());
 
         map.selectAll(".subunit")
-            .data(topojson.feature(o, o.objects.hokkaido).features)
+            .data(topojson.feature(topo, topo.objects.hokkaido).features)
             .enter()
             .append("path")
             .attr('id', function (d) {
                 return d.id;
             })
             .attr('class', function (d) {
-                //console.log(quantizev2(populationById.get(d.id)));
-                return "subunit " + quantize($scope.populationDeltaChange.get(d.id));
-                //return "subunit";
+                var val = $scope.populationDeltaChange.get(d.id);
+                if (val!==undefined){
+                    //console.log(val[1]);
+                    return "subunit " + quantize(val[1]);
+                }
             })
             .style('fill', function (d) {
                 var val = $scope.populationDeltaChange.get(d.id);
-                if (val > 0) {
-                    return quantizePositive(val);
+
+                if (val===undefined){
+                    return "#ddc";
+                }
+                if (val[1] > 0) {
+                    return quantizePositive(val[1]);
                 } else {
-                    return quantizeNegative(val);
+                    return quantizeNegative(val[1]);
                 }
 
             })
